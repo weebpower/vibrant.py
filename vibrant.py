c1c0dedcbdf99524411d4499569a69ae024d57ce
edeak <edeak@null.net>
2017-07-22 18:37:13 +0100

    Now works.

--- vibrant.py
+++ vibrant.py
@@ -1,55 +1,63 @@
-
 #!/usr/bin/env python3
 
 from colorthief import ColorThief
-from sys import argv
+from sys import argv, exit
 from PIL import Image
 from colour import Color
 
-MIN_LUM = 0.5
-MAX_LUM = 0.9
-MIN_SAT = 0.5
+MIN_LUM = 0.3
+MAX_LUM = 0.7
+MIN_SAT = 0.35
 WEIGHT_SAT = 3
 WEIGHT_LUM = 6
 WEIGHT_POP = 1
 
-class Pixel(Color):
+class Pixel():
     def __init__(self, population, color=None, **kwargs):
-        super().__init__(color, **kwargs) 
+        self.color = Color(color, **kwargs)
         self.population = population
-    def isvibrant(self):
-        return MIN_SAT <= self.saturation and MIN_LUM <= self.luminance <= MAX_LUM
+    def __repr__(self):
+        return self.color.hex
     def vibrance(self):
-        return self.population * WEIGHT_POP + self.saturation * WEIGHT_SAT + self.luminance * WEIGHT_LUM
-    
-def rawcolors_to_objects(imagemap):
-    return [Pixel(pixel[1], rgb=pixel[2]) for pixel in imagemap]
+        return MIN_SAT <= self.color.saturation and MIN_LUM <= self.color.luminance <= MAX_LUM and \
+               self.population * WEIGHT_POP + self.color.saturation * WEIGHT_SAT + \
+               self.color.luminance * WEIGHT_LUM
 
-def read_hexcodes(file):
-    return file.read().splitlines()
+def usage():
+    print('USAGE:', __file__ + ' [-a] IMAGE')
+    exit(1)
 
-def get_palette(imagename):
+def abs_to_rel(rgb):
+    return tuple(c / 255 for c in rgb)
+
+def get_palette(filename):
     ct = ColorThief(argv[1])
     palette = ct.get_palette(quality=1, color_count=32)
-    return [Pixel(1, rgb=rgb) for rgb in palette]
+    return [Pixel(1, rgb=abs_to_rel(rgb)) for rgb in palette]
 
-def get_colors(imagename):
-    im = Image.open(filename).convert('RGB') 
-    im.getcolors(0xFFFFFF)
+def get_allcolors(filename):
+    im = Image.open(filename).convert('RGB')
+    imagemap = im.getcolors(0xFFFFFF)
+    im.close()
+    return [Pixel(pixel[0], rgb=abs_to_rel(pixel[1])) for pixel in imagemap]
 
 def mostvibrant(colorlist):
     return max(colorlist, key=lambda c: c.vibrance())
 
-def mostvibrant_palette(imagename):
-    return mostvibrant(get_palette(imagename))
+def mostvibrant_palette(filename):
+    return mostvibrant(get_palette(filename))
 
-def mostvibrant_allcolors(imagename):
-    return mostvibrant(get_colorcount(imagename))
+def mostvibrant_allcolors(filename):
+    return mostvibrant(get_allcolors(filename))
 
-def main(imagename):
-    mostvibrant_allcolors(imagename)
+def main():
+    if len(argv) < 2:
+        usage()
+    if '-a' in argv:
+        argv.remove('-a')
+        print(mostvibrant_allcolors(argv[1]))
+    else:
+        print(mostvibrant_palette(argv[1]))
 
 if __name__ == '__main__':
-    if len(argv) < 2:
-        raise SystemExit('Pass an image as argument.')
-    main(argv[1])
+    main()
