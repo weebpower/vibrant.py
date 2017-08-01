51e359e6aa9a9866089a895857865cf262d31f70
edeak <edeak@null.net>
2017-08-01 17:22:28 +0100

    Made allcolors the default algorithm. Made improvements to the code. Added comments.

--- vibrant.py
+++ vibrant.py
@@ -1,66 +1,80 @@
 #!/usr/bin/env python3
 
-from colorthief import ColorThief
 from sys import argv, exit
-from PIL import Image
 from colour import Color
 
-MIN_LUM = 0.3
-MAX_LUM = 0.6
-MIN_SAT = 0.5
-#TARGET_LUM = 0.5
-WEIGHT_SAT = 4.0
-WEIGHT_LUM = 6.0
-WEIGHT_POP = 0.5
-PALETTE_QUALITY = 32 # from 1 to inf. Higher is worse.
-PALETTE_COLOR_COUNT = 16 # from 1 to inf.
+MINLUM = 0.3
+MAXLUM = 0.7
+MINSAT = 0.5
+WEIGHTSAT = 4.0
+WEIGHTLUM = 6.0
+WEIGHTPOP = 0.5
 
+# Used to determine the most vibrant color.
 class Pixel():
     def __init__(self, population, color=None, **kwargs):
-        self.color = Color(color, **kwargs)
-        self.population = population
+        self.color = Color(color, **kwargs) # see the colour module
+        self.population = population        # times it appears in an image
     def __repr__(self):
         return self.color.hex_l
     def vibrance(self):
-        return MIN_SAT <= self.color.saturation and MIN_LUM <= self.color.luminance <= MAX_LUM and \
-               self.population * WEIGHT_POP + self.color.saturation * WEIGHT_SAT + \
-               (1 - abs(2 * self.color.luminance - 1))  * WEIGHT_LUM
+        return MINSAT <= self.color.saturation and \
+	       MINLUM <= self.color.luminance <= MAXLUM and \
+               WEIGHTPOP * self.population + \
+	       WEIGHTSAT * self.color.saturation + \
+	       WEIGHTLUM * absmax1athalf(self.color.luminance) 
 
+# An upside down absolute value function with an absolute maximum of (0.5, 1).
+def absmax1athalf(x):
+    return (1 - abs(2 * x - 1))
+
+# Program usage. Trivial.
 def usage():
-    print('USAGE:', __file__ + ' [-a] IMAGE')
+    print('USAGE:', __file__, '[-p] IMAGE')
     exit(1)
 
+# Take an rgb color that ranges in [0, 1] and make it range in [0, 255]
 def abs_to_rel(rgb):
     return tuple(c / 255 for c in rgb)
 
+# Get a palette from ColorThief, then return it as a list of Pixel objects.
 def get_palette(filename, quality, color_count):
+    from colorthief import ColorThief
     ct = ColorThief(filename)
     palette = ct.get_palette(quality=quality, color_count=color_count)
     return [Pixel(1, rgb=abs_to_rel(rgb)) for rgb in palette]
 
+# Get all colors from the image and the times they appear using PIL,
+# then return them as a list of Pixel objects.
 def get_allcolors(filename):
+    from PIL import Image
     im = Image.open(filename).convert('RGB')
     imagemap = im.getcolors(0xFFFFFF)
     im.close()
     return [Pixel(pixel[0], rgb=abs_to_rel(pixel[1])) for pixel in imagemap]
 
+# Return the most vibrant color out of an iterable.
 def mostvibrant(colorlist):
     return max(colorlist, key=lambda c: c.vibrance())
 
-def mostvibrant_palette(filename)):
+# The worst but fastest algorithm: get the most vibrant color out of a palette.
+def mostvibrant_palette(filename):
+    PALETTE_QUALITY = 32     # from 1 to inf. Higher is worse.
+    PALETTE_COLOR_COUNT = 32 # from 1 to inf.
     return mostvibrant(get_palette(filename, PALETTE_QUALITY, PALETTE_COLOR_COUNT))
 
+# The best but slowest algorithm: get the most vibrant color out of all pixels.
 def mostvibrant_allcolors(filename):
     return mostvibrant(get_allcolors(filename))
 
 def main():
-    if len(argv) < 2:
-        usage()
-    elif '-a' in argv:
-        argv.remove('-a')
+    if len(argv) is 2:
         print(mostvibrant_allcolors(argv[1]))
-    else:
+    elif len(argv) is 3 and '-p' in argv:
+        argv.remove('-p')
         print(mostvibrant_palette(argv[1]))
+    else:
+        usage()
 
 if __name__ == '__main__':
     main()
